ad0739260e5acab45f775178bc90363992970a4f
Shaina C <scailles23@gmail.com>
2020-01-26 05:03:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  
+  @@all_dogs=[]
+  
+  def initialize(name, owner, mood="nervous")
+    @name=name
+    @owner=owner
+    @mood=mood
+    @@all_dogs << self
+  end
+  
+  def self.all
+  @@all_dogs
+  end
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,46 @@
 class Owner
-  # code goes here
+  
+  attr_accessor :pets, :cat, :dog
+  
+  attr_reader :name, :species
+  
+  @@all=[]
+  
+  def initialize(species="human",name)
+    @species=species
+    @name=name
+    @@all << self
+    @cats=[]
+    @dogs=[]
+  end
+  
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+   @@all.clear
+  end
+  
+  def cats
+  Cat.all.select {|cat| cat.owner == self}
+  end
+  
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+  
+  def buy_cat(cat)
+    Cat.new(cat)
+  end
+    
+  
 end
